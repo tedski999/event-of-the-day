fdd62776b030594f71d1fd003b7ef64139c93764
tedski999 <tedjohnsonjs@gmail.com>
2020-09-02 02:50:15 +0100

    Added HTML parsing and storing JSON to disk
    Downloaded Wikipedia articles are scraped for their events list
    The scraped data is stored on disk in JSON format

--- eventoftheday.py
+++ eventoftheday.py
@@ -1,5 +1,8 @@
+import os
 import sys
 import time
+import json
+import re
 import argparse
 import datetime
 import calendar
@@ -8,7 +11,8 @@ import appdirs
 from bs4 import BeautifulSoup
 
 WIKIPEDIA_ARTICLE_ADDR = "https://en.wikipedia.org/wiki/"
-EVENTS_FILEPATH = appdirs.user_data_dir("eventoftheday", "tedski999") + "/events"
+USER_DATA_DIR = appdirs.user_data_dir("eventoftheday", "tedski999")
+EVENTS_FILEPATH = USER_DATA_DIR + "/events"
 VALID_LEAP_YEAR = 2012
 
 def main():
@@ -38,34 +42,62 @@ def print_random_event(args):
 def print_events(args):
     print("TODO: list events")
 
+# Scrapes all of the Wikipedia day articles for historic events
 def download_events(args):
-    all_events = []
+    print("Downloading all historic events listed on the Wikipedia day articles...")
+
+    # Loop through every month
+    all_events = {}
     for month in range(1, 13):
+
+        # Loop through every day in the month
         month_name = calendar.month_name[month]
         month_length = calendar.monthrange(VALID_LEAP_YEAR, month)[1]
-        month_events = []
+        month_events = {}
         for day in range(1, month_length + 1):
+            day_events = []
+
+            # Download and begin parsing the Wikipedia page
+            # TODO: error handling
             time.sleep(0.01)
             address_string = WIKIPEDIA_ARTICLE_ADDR + month_name + "_" + str(day)
-            print("Scrapping all historic events from " + address_string)
+            print("Scraping " + address_string + "...")
             page = requests.get(address_string)
             soup = BeautifulSoup(page.content, "html.parser")
-            event_header = soup.find_all("span", class_="mw-headline", id="Events")[0] # TODO: parent is needed (this is the span)
-            # TODO: keep getting next from event_header header until the next class_="mw-headline" is reached, keeping and parsing "li" tags only
-            # TODO: save parsed and formatted "li" tags to month_events
 
-        all_events.append(month_events)
+            # Locate and loop through every event on the page
+            year_events_tag = soup.find("span", class_="mw-headline", id="Events").parent
+            next_tag = year_events_tag.next_sibling.next_sibling
+            while next_tag.name != "h2":
+                if next_tag.name == "ul":
+                    for next_list_item in next_tag.find_all("li"):
 
-    # TODO: write all the scrapped events to EVENTS_FILEPATH, formatted as:
-    #
-    # <MONTH NAME>_<DAY>:
-    # <EVENT 1 YEAR>: <EVENT 1 DESCRIPTION>
-    # <EVENT 2 YEAR>: <EVENT 2 DESCRIPTION>
-    # etc..
-    #
-    # <MONTH NAME>_<DAY>:
-    # etc...
-    #
+                        # Remove citation tags
+                        for sup_tag in next_list_item("sup"):
+                            sup_tag.decompose()
+
+                        # Parse valid text and save to day_events
+                        text = next_list_item.get_text()
+                        event_data = text.split(" – ")
+                        if len(event_data) == 2 and re.match(r"^[0-9]+\s+((BC|AD)\s+)?–.+$", text):
+                            day_events.append(event_data[0].strip() + " – " + event_data[1].strip())
+
+                # Go to the next tag
+                next_tag = next_tag.next_sibling.next_sibling
+
+            # Save all the parsed day events to a key on the month_events object
+            month_events[str(day)] = day_events
+
+        # Save the month object to a key on the all_events object
+        all_events[month_name] = month_events
+
+    # Write events to a JSON file
+    print("Writing parsed events data to disk...")
+    data = json.dumps(all_events, indent=4)
+    if not os.path.isdir(USER_DATA_DIR):
+        os.mkdir(USER_DATA_DIR)
+    with open(EVENTS_FILEPATH, "w") as events_file:
+        events_file.write(data)
 
 main()
 
