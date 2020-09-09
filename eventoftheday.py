9e17a41b9f0a8fce4fd59c0feeebdf64191a9622
tedski999 <tedjohnsonjs@gmail.com>
2020-09-09 16:55:46 +0100

    Performance optimizations
    Each day of the year has a dedicated file for their events
    Events no long need JSON formatting
    Modules are only imported when needed
    HTTP requests now use a 5 second timeout

--- eventoftheday.py
+++ eventoftheday.py
@@ -3,20 +3,13 @@
 import os
 import sys
 import signal
-import time
-import random
-import json
-import re
 import argparse
 import datetime
 import calendar
-import requests
 import appdirs
-from bs4 import BeautifulSoup
 
 WIKIPEDIA_ARTICLE_ADDR = "https://en.wikipedia.org/wiki/"
 USER_DATA_DIR = appdirs.user_data_dir("eventoftheday", "tedski999")
-EVENTS_FILEPATH = USER_DATA_DIR + "/events"
 VALID_LEAP_YEAR = 2012
 
 def main():
@@ -44,6 +37,7 @@ def main():
 
 # Print a randomly selected historic event for the date
 def print_random_event(args):
+    import random
     day_suffix = ["th", "st", "nd", "rd", "th"][min(args.date.day % 10, 4)]
     if 11 <= (args.date.day % 100) <= 13:
         day_suffix = "th"
@@ -58,14 +52,17 @@ def print_events(args):
 def download_events(args):
     print("Downloading all historic events listed on the Wikipedia day articles...")
 
+    import re
+    import time
+    import requests
+    from bs4 import BeautifulSoup
+
     # Loop through every month
-    all_events = {}
     for month in range(1, 13):
 
         # Loop through every day in the month
         month_name = calendar.month_name[month]
         month_length = calendar.monthrange(VALID_LEAP_YEAR, month)[1]
-        month_events = {}
         for day in range(1, month_length + 1):
             day_events = []
 
@@ -74,7 +71,7 @@ def download_events(args):
             address_string = WIKIPEDIA_ARTICLE_ADDR + month_name + "_" + str(day)
             print("Scraping {0}...".format(address_string))
             try:
-                page = requests.get(address_string)
+                page = requests.get(address_string, timeout=5)
                 soup = BeautifulSoup(page.content, "html.parser")
             except requests.exceptions.RequestException as err:
                 sys.stderr.write("An error occurred while trying to download {0}:\n{1}".format(address_string, err))
@@ -100,40 +97,35 @@ def download_events(args):
                 # Go to the next tag
                 next_tag = next_tag.next_sibling.next_sibling
 
-            # Save all the parsed day events to a key on the month_events object
-            month_events[str(day)] = day_events
+            # Save all the parsed day events to disk
+            day_file_path = "{0}/{1}_{2}".format(USER_DATA_DIR, month_name, day)
+            if not os.path.isdir(USER_DATA_DIR):
+                os.mkdir(USER_DATA_DIR)
+            with open(day_file_path, "w") as day_file:
+                day_file.write("\n".join(day_events))
 
-        # Save the month object to a key on the all_events object
-        all_events[month_name] = month_events
-
-    # Write events to a JSON file
-    print("Writing parsed events data to disk...")
-    data = json.dumps(all_events, indent=4)
-    if not os.path.isdir(USER_DATA_DIR):
-        os.mkdir(USER_DATA_DIR)
-    with open(EVENTS_FILEPATH, "w") as events_file:
-        events_file.write(data)
+    print("Done!")
 
 # Parse a datetime object from a provided string
 def date(date_string):
     date = datetime.datetime.strptime(date_string, "%m/%d")
     return date
 
-# Read in JSON data from events file and return array of historic events for month and day
+# Read and return a list of historic events for month and day
 def get_day_events(month, day):
 
     # Attempt to open the events data file
+    day_file_path = "{0}/{1}_{2}".format(USER_DATA_DIR, month, day)
     try:
-        events_file = open(EVENTS_FILEPATH, "r")
+        day_file = open(day_file_path, "r")
     except OSError:
-        sys.stderr.write("Could not open events data: {0}\nHave you downloaded the events from Wikipedia with 'eventoftheday download'?\n".format(EVENTS_FILEPATH))
+        sys.stderr.write("Could not open day events data: {0}\nHave you downloaded the events from Wikipedia with 'eventoftheday download'?\n".format(day_file_path))
         quit(1)
 
-    # Load JSON object from file
-    with events_file:
-        events_data = json.load(events_file)
-        day_events = events_data[month]
-        day_events = day_events[day]
+    # Load events from disk
+    with day_file:
+        day_events = [line.rstrip() for line in day_file]
+
     return day_events
 
 # Exit gracefully when receiving SIGINT
